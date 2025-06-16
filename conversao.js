b02eabc51b567d5c853c8f5913d20b834e848a12
Isaque Gomes <114315861+IsaqueGomesDev@users.noreply.github.com>
2025-06-16 07:42:49 -0300

    Delete conversao.js

--- conversao.js
+++
@@ -1,8 +0,0 @@
-const precoInput = document.getElementById('preco');
-
-  precoInput.addEventListener('input', function (e) {
-    let value = e.target.value.replace(/\D/g, ''); 
-    value = (parseInt(value) / 100).toFixed(2); 
-    e.target.value = value
-      .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-  });
